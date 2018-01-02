ebeee28ce752481aecfc76054182c8de28a8efc6
Erin Witmer <ecwitmer@gmail.com>
2018-01-02 02:36:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |item|
-if my_condition_is_met
-  counter = counter + 1
-end
+    if my_condition_is_met
+      counter = counter + 1
+    end
 end
